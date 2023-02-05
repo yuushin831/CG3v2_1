9eafd138873b151ed98aafc0c79a82e8515cf2de
takei_yuushin0831 <k021g1262@g.neec.ac.jp>
2023-02-05 18:32:28 +0900

    プロジェクト ファイルを追加します。

---
+++ Resources/shaders/OBJVertexShader.hlsl
@@ -0,0 +1,19 @@
+#include "OBJShaderHeader.hlsli"
+
+VSOutput main(float4 pos : POSITION, float3 normal : NORMAL, float2 uv : TEXCOORD)
+{
+	//右、下、奥の方向を向いたライト
+	float3 lightdir = float3(1, -1, 1);
+	lightdir = normalize(lightdir);
+	//ライトの色(白)
+	float3 lightcolor = float3(1, 1, 1);
+	// ピクセルシェーダーに渡す値
+	VSOutput output; 
+	output.svpos = mul(mat,pos);
+	//Lambert反射の計算
+	output.color.rgb = dot(-lightdir, normal) * m_diffuse * lightcolor;
+	output.color.a = m_alpha;
+	output.uv = uv;
+
+	return output;
+}
